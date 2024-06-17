daf3a0888b07c07bb124fa72f3a894c64d376248
manchalaharikesh <manchalaharikesh@gmail.com>
2024-06-17 20:50:16 +0530

    GFG Commit-2 on 17-JUN-2024

---
+++ GeeksForGeeks/Check If two Line segments Intersect.cpp
@@ -0,0 +1,56 @@
+// DATE: 17-JUN-2024
+// Question URL: https://www.geeksforgeeks.org/problems/check-if-two-line-segments-intersect0017/1
+// Question: Check If two Line segments Intersect
+// Description: Given the coordinates of the endpoints(p1,q1, and p2,q2) of the two line segments. Check if they intersect or not. If the Line segments intersect return true otherwise return false.
+
+//              Note: Please check the intersection lies within the line segments.
+// Approach: 1. Code is Self-Explanatory.
+
+class Solution {
+  public:
+    string doIntersect(int p1[], int q1[], int p2[], int q2[]) {
+        float m1, m2;
+        float x, y;
+        bool per1 = false, per2 = false;
+        
+        if(p1[0] == q1[0]){ 
+            m1 = 0;
+            per1 = true;
+        }
+        else m1 = (q1[1] - p1[1]) / (float)(q1[0] - p1[0]);
+        
+        if(p2[0] == q2[0]){ 
+            m1 = 0;
+            per2 = true;
+        }
+        else m2 = (q2[1] - p2[1]) / (float)(q2[0] - p2[0]);
+        
+        if(m1 == m2){ 
+            // lines parallel to y-axis slope tan90
+            if(per1){
+                return p1[0] == p2[0] ? "true" : "false";
+            }
+            
+            // lines parallel to x-axis slope tan0
+            return p1[1] == p2[1] ? "true" : "false";
+        }
+        
+        // (y - y1) = m1 * (x - x1);
+        // (y - y2) = m2 * (x - x2);
+        // y1 + m1 * (x - x1) = y2 + m2 * (x - x2)
+        // m1*x - m1*x1 - m2*x + m2*x2 = y2 - y1
+        // x*(m1 - m2) = (y2 - y1) + m1*x1 - m2*x2
+        // x = ((y2-y1)+(m1*x1 - m2*x2))/(m1 - m2)
+        
+        x = per1 ? p1[0] : per2 ? p2[0] : ((p2[1] - p1[1]) + (m1*p1[0] - m2*p2[0]))/(m1-m2);
+        y = p1[1] + m1 * (x - p1[0]);
+        
+        if(min(p1[0], q1[0]) <= x && x <= max(p1[0], q1[0]) && min(p1[1], q1[1]) <= y && y <= max(p1[1], q1[1])){
+            if(min(p2[0], q2[0]) <= x && x <= max(p2[0], q2[0]) && min(p2[1], q2[1]) <= y && y <= max(p2[1], q2[1])){
+                return "true";
+            }
+        }
+        
+        return "false";
+    }
+};
