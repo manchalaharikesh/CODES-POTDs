3879d98f068fd99e957dd7ae0e91a034635469f9
manchalaharikesh <manchalaharikesh@gmail.com>
2024-06-27 07:07:58 +0530

    GFG Commit-1 on 27-JUN-2024

--- GeeksForGeeks/Toeplitz matrix.cpp
+++ GeeksForGeeks/Toeplitz matrix.cpp
@@ -3,7 +3,7 @@
 
 // Question: Toeplitz matrix
 // Description: A Toeplitz (or diagonal-constant) matrix is a matrix in which each descending diagonal from left to right is constant, i.e., all elements in a diagonal are the same. Given a rectangular matrix mat, your task is to complete the function isToeplitz which returns 1 if the matrix is Toeplitz otherwise, it returns 0.
-// Approach: 1. Code is Self-Explanatory, Dry Run the code for better understanding.
+// Approach: 1. We just need to traver the matrix diagonally, Dry Run the code for better understanding.
 
 bool isToepliz(vector<vector<int>>& mat) {
     int k = 0, ele;
